d82880b7f257f95fa1450d539621be987a0c534e
avasawni17 <akshaykeswani17@gmail.com>
2021-01-10 21:47:38 +0530

    initial commmit

--- Assignment2/BillofMaterial/src/main/scala/BillOfMaterial.scala
+++ Assignment2/BillofMaterial/src/main/scala/BillOfMaterial.scala
@@ -53,6 +53,14 @@ object BillOfMaterial extends App {
       case Textbox(x,y,w,h,t) => if(x>0 && y>0 && w>0 && h>0) true else throw new Exception("")
     }
   }
+
+  print(generateBill(rectangle = Some(Rectangle(10,10,30,40)),
+    square = Some(Square(15,30,35)),
+    ellipse = Some(Ellipse(100,150,300,200)),
+    circle = Some(Circle(1,1,300)),
+    textbox = Some(Textbox(5,5,100,200,Some("sample text")))))
+
+//  println(generateBill(rectangle = Some(Rectangle(-10,10,30,40))))
 }
 
 
